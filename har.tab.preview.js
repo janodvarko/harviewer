42f8ee24e43d85956b7a9e10ca8df66624a7b083
odvarko@gmail.com <odvarko@gmail.com@b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b>
2010-03-04 18:23:23 +0000

    Small fixes (safer condition + improve CSS)
    git-svn-id: http://harviewer.googlecode.com/svn/trunk@79 b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b

--- har.tab.preview.js
+++ har.tab.preview.js
@@ -324,7 +324,7 @@ HAR.Tab.Preview = HAR.extend(
         var timeText = formatTime(totalTime);
 
         // xxxHonza: localization?
-        if (page && page.pageTimings.onLoad)
+        if (page && page.pageTimings.onLoad > 0)
             timeText += " (onload: " + formatTime(page.pageTimings.onLoad) + ")";
 
         timeLabel.innerHTML = timeText;
