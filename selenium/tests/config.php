01479a915ac24918c221994226cda93f270d4bb6
odvarko@gmail.com <odvarko@gmail.com@b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b>
2010-10-05 21:19:31 +0000

    Fix selenium config.
    git-svn-id: http://harviewer.googlecode.com/svn/trunk@165 b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b

--- selenium/tests/config.php
+++ selenium/tests/config.php
@@ -1,5 +1,5 @@
 <?php 
 # don't forget the trailing slash
-$harviewer_base = "http://legoas/har/viewer-2.0/webapp-build/";
-$test_base = "http://legoas/har/viewer-2.0/selenium/";
+$harviewer_base = "http://legoas/har/viewer/webapp-build/";
+$test_base = "http://legoas/har/viewer/selenium/";
 ?>
