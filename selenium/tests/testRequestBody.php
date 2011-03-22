6a5e660b625e09f937505d495b6e3d247a3225c8
odvarko@gmail.com <odvarko@gmail.com@b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b>
2011-03-22 19:33:56 +0000

    Selenium test for issue 45
    git-svn-id: http://harviewer.googlecode.com/svn/trunk@279 b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b

--- selenium/tests/testRequestBody.php
+++ selenium/tests/testRequestBody.php
@@ -36,5 +36,31 @@ class HAR_TestRequestBody extends HAR_TestCase
         $this->assertElementContainsText("css=.tabParamsBody.tabBody.selected ",
             "value11value22value33");
     }
+
+    public function testDataURL()
+    {
+        print "\nHAR Test Data URL";
+
+        $viewer_base = $GLOBALS["harviewer_base"];
+        $test_base = $GLOBALS["test_base"];
+
+        $url = $viewer_base."?path=".$test_base."tests/hars/data-url.har";
+        $this->open($url);
+
+        // The Preview tab must be selected
+        $this->assertElementContainsText("css=.PreviewTab.selected", "Preview");
+
+        // There must be one page (expanded).
+        $this->assertElementContainsText("css=.pageRow.opened", "http://www.test.com/");
+
+        // Expand the only request entry.
+        $this->click("css=.netFullHrefLabel.netHrefLabel.netLabel");
+
+        $this->assertElementExists("css=.netInfoRow");
+        $this->click("css=.DataURLTab.tab");
+
+        $this->assertElementContainsText("css=.tabDataURLBody.tabBody.selected ",
+            "data:text/css;charset=utf-8,body{text-align:center;}");
+    }
 }
 ?>
