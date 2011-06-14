cf7e48f6aee3f9190afd968ed8678608f80b5317
odvarko@gmail.com <odvarko@gmail.com@b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b>
2011-06-14 07:09:15 +0000

    Issue 48: Patch: add events to the Preview application as well
    git-svn-id: http://harviewer.googlecode.com/svn/trunk@303 b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b

--- webapp/scripts/harPreview.js
+++ webapp/scripts/harPreview.js
@@ -2,21 +2,56 @@
 
 require.def("harPreview", [
     "preview/pageList",
-    "preview/harModel"
+    "preview/harModel",
+    "core/lib",
+    "core/trace"
 ],
 
-function(PageList, HarModel) {
+function(PageList, HarModel, Lib, Trace) {
 
 //*************************************************************************************************
 // The Preview Application
 
-// Load HAR file if any is specified in the URL.
-HarModel.Loader.run(function onHarLoad(jsonString)
+function HarPreview()
 {
-    var input = HarModel.parse(jsonString, true);
-    var pageList = new PageList(input);
-    pageList.render(document.getElementById("content"));
-});
+    this.id = "harPreview";
+
+    this.model = new HarModel();
+}
+
+HarPreview.prototype =
+{
+    initialize: function(content)
+    {
+        // Auto load all HAR files specified in the URL.
+        var okCallback = Lib.bind(this.appendPreview, this);
+        HarModel.Loader.run(okCallback);
+    },
+
+    appendPreview: function(jsonString)
+    {
+        var input = HarModel.parse(jsonString, true);
+        this.model.append(input);
+
+        var pageList = new PageList(input);
+        pageList.render(content);
+
+        Lib.fireEvent(content, "onPreviewHARLoaded");
+    }
+}
+
+//*************************************************************************************************
+// Initialization
+
+var content = document.getElementById("content");
+var harPreview = content.repObject = new HarPreview();
+
+// Fire some events for listeners. This is useful for extending/customizing the viewer.
+Lib.fireEvent(content, "onPreviewPreInit");
+harPreview.initialize(content);
+Lib.fireEvent(content, "onPreviewInit");
+
+Trace.log("HarPreview; initialized OK");
 
 //*************************************************************************************************
 });
