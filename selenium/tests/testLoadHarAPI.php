5d25fa2d29a425adec47c3137630764c9d4f3a2b
odvarko@gmail.com <odvarko@gmail.com@b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b>
2011-07-05 11:08:03 +0000

    Missing test file
    git-svn-id: http://harviewer.googlecode.com/svn/trunk@326 b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b

---
+++ selenium/tests/testLoadHarAPI.php
@@ -0,0 +1,43 @@
+<?php
+require_once("HARTestCase.php");
+
+/**
+ * Check pageTimings fields. Both these fields (onLoad, onContentLoad)
+ * are optional and can be omitted.
+ */ 
+class HAR_TestLoadHarAPI extends HAR_TestCase
+{
+    public function testViewer()
+    {
+        print "\nTest loadHAR API in the Viewer";
+
+        // Open customized viewer.
+        $test_base = $GLOBALS["test_base"];
+        $this->open($GLOBALS["test_base"]."tests/testLoadHarAPIViewer.html");
+
+        // Wait for 10 sec to load HAR files.
+        $this->waitForCondition(
+            "selenium.browserbot.getCurrentWindow().document.querySelectorAll('.pageTable').length == 3",
+            10000);
+
+        $this->assertElementContainsText("css=.PreviewTab.selected", "Preview");
+        $this->assertEquals(3, $this->getCssCount(".pageTable"));
+    }
+
+    public function testPreview()
+    {
+        print "\nTest loadHAR API in the Preview";
+
+        // Open customized preview.
+        $test_base = $GLOBALS["test_base"];
+        $this->open($GLOBALS["test_base"]."tests/testLoadHarAPIPreview.html");
+
+        // Wait for 10 sec to load HAR files.
+        $this->waitForCondition(
+            "selenium.browserbot.getCurrentWindow().document.querySelectorAll('.pageTable').length == 3",
+            10000);
+
+        $this->assertEquals(3, $this->getCssCount(".pageTable"));
+    }
+}
+?>
