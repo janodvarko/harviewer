9538ac6ae358543ad46706d7e9ef4dce5ac39725
odvarko@gmail.com <odvarko@gmail.com@b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b>
2011-07-15 20:12:58 +0000

    Embedding HAR previews in a page.
    git-svn-id: http://harviewer.googlecode.com/svn/trunk@338 b08ab4b8-96ea-11de-a1ee-bbbdd2d7798b

---
+++ webapp/har.js
@@ -0,0 +1,121 @@
+/* See license.txt for terms of usage */
+
+(function() {
+// ********************************************************************************************* //
+
+/**
+ * Finds all elements with class="har" on the page and creates HAR preview frame for each.
+ * See list of attributes that can be specified on such elements:
+ * 
+ * class: (mandatory) elements with this class are considered as HAR preview elements.
+ *      The class is removed as soon as the element is processed. Possible additional
+ *      classes are not touched.
+ * data-har: (mandatory) source URL for target file.
+ *      1) The URL must start with 'http:' in case where the target file is located on
+ *         a different domain. The file must use HARP syntax (JSONP) in these cases.
+ *      2) Otherwise the URL is considered to be from the same domain and path relative to
+ *         the current location). The file must use HAR syntax (JSON) in these cases.
+ * width: (optional, default: '100%') width of the preview.
+ * height: (optional, default: '150px') height of the preview.
+ * expand: (optional, default: 'true') true if individual pages should be expanded.
+ * 
+ * Embed this script on a page:
+ * <script>
+ * (function() {
+ *     var har = document.createElement("script");
+ *     har.src = "http://www.softwareishard.com/har/viewer/har.js";
+ *     har.setAttribute("id", "har");
+ *     har.setAttribute("async", "true");
+ *     document.documentElement.firstChild.appendChild(har);
+ * })();
+ * if(typeof(harInitialize)!="undefined"){harInitialize()}
+ * </script>
+ * 
+ * Examples of HAR elements:
+ * <div class="har" data-har="http://example.como/my.harp"></div>
+ * - load HARP file from an external domain using JSONP.
+ * 
+ * <div class="har" data-har="/example.como/my.harp"></div>
+ * - load HAR file from the same domain.
+
+ * <div class="har" data-har="http://example.como/my.harp"></div>
+ * - load HAR file from an external domain and expads all pages.
+ */
+window.harInitialize = function()
+{
+    var script = document.getElementById("har");
+    var index = script.src.lastIndexOf("/");
+    var baseUrl = script.src.substr(0, index+1);
+
+    var elements = findHarElements();
+    for (var i=0; i<elements.length; i++)
+    {
+        var element = elements[i];
+        var path = element.getAttribute("data-har");
+        if (!path)
+            continue;
+
+        var width = element.getAttribute("width");
+        var height = element.getAttribute("height");
+        var expand = element.getAttribute("expand");
+
+        var args = "?" + (path.indexOf("http:") == 0 ? "inputUrl" : "path") + "=" + path;
+        if (expand != "false")
+            args += "&expand=" + (expand ? expand : "true");
+
+        var iframe = document.createElement("iframe");
+        iframe.setAttribute("style", "border: 1px solid lightgray;");
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("width", width ? width : "100%");
+        iframe.setAttribute("height", height ? height : "150px");
+        iframe.setAttribute("src", baseUrl + "preview.php" + args);
+        element.appendChild(iframe);
+
+        removeHarClass(element);
+    }
+}
+
+var re = new RegExp('(^|\\s)har(\\s|$)', "g");
+
+/**
+ * Removes "har" class from specified element so it isn't processed twice.
+ * @param {Object} node to be marked as processed
+ */
+function removeHarClass(node)
+{
+    var className = node.className.replace(re, " ");
+    node.className = className.replace(/^\s*|\s*$/g,"");
+}
+
+/**
+ * Returns list of all elements with class="har" on the page.
+ */
+function findHarElements()
+{
+    var elements = [];
+    if (document.getElementsByClassName)
+    {
+        elements = document.getElementsByClassName("har");
+    }
+    else if (document.getElementsByTagName)
+    {
+        var divs = document.getElementsByTagName("div");
+        for (var i=0; i<divs.length; i++)
+        {
+            var cn = divs[i].className;
+            if (cn && cn.match(re))
+                elements.push(divs[i]);
+        }
+    }
+
+    var result = [];
+    for (var i=0; i<elements.length; i++)
+        result.push(elements[i]);
+
+    return result;
+}
+
+harInitialize();
+
+// ********************************************************************************************* //
+})();
