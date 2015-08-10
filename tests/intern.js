728b0fc8e39e854e84711b56b1e7916cba80be9c
gitgrimbo <gitgrimbo@gmail.com>
2015-08-10 22:00:00 +0100

    Start to add code coverage reports
    - Add Lcov and LcovHtml reporters to intern config.
    - Add new 'tests/unit/core/lib' and 'tests/unit/core/cookies' unit tests.
    
    /lcov.info file and /html-report/ folder will be generated.
    
    For the code coverage to work with functional tests, the proxyUrl in the
    Intern config needs to be a URL that is reachable from the Selenium test
    nodes (i.e. not localhost if the Selenium nodes are on a different host to
    the Intern proxy).
    
    Both the $harviewer_base and $test_base URLs in config.php, and the
    harviewer.harViewerBase and harviewer.testBase URLs in the Intern config
    need to point to the proxyUrl to get coverage information.
    
    E.g. (in /selenium/tests/config.php):
    
    $harviewer_base = "http://192.168.1.80:9000/webapp/";
    $test_base = "http://192.168.1.80:9000/selenium/";
    
    and (in /tests/intern.js):
    
        harviewer: {
            harViewerBase: 'http://192.168.1.80:9000/webapp/',
            testBase: 'http://192.168.1.80:9000/selenium/'
        },
    
    Config value `excludeInstrumentation` has also been improved to exclude
    all code except the application code.

--- tests/intern.js
+++ tests/intern.js
@@ -48,6 +48,8 @@ define([
     //reporters: ['runner'],
     reporters: [
       'Pretty',
+      'Lcov',
+      'LcovHtml',
       { 'id': 'JUnit', 'filename': 'report.xml' }
     ],
     //reporters: ['console', 'junit'],
@@ -55,11 +57,15 @@ define([
 
     // Configuration options for the module loader; any AMD configuration options supported by the Dojo loader can be
     // used here
-    loaderOptions: {},
+    loaderOptions: {
+      packages: [{name: 'core', location: 'webapp/scripts/core'}]
+    },
 
     // Non-functional test suite(s) to run in each browser
     suites: [
-      'intern/node_modules/dojo/has!host-browser?tests/unit/unit'
+      'intern/node_modules/dojo/has!host-browser?tests/unit/unit',
+      'tests/unit/core/lib',
+      'tests/unit/core/cookies'
     ],
 
     // Functional test suite(s) to run in each browser once non-functional tests are completed
@@ -91,8 +97,41 @@ define([
       'tests/functional/testSearchJsonQuery'
     ],
 
-    // A regular expression matching URLs to files that should not be included in code coverage analysis
-    excludeInstrumentation: /^intern|tests|bower_components|node_modules[\\/]/
+    // A regular expression matching URLs to files that should not be included
+    // in code coverage analysis.
+
+    // Some of the patterns listed here can also be used in a reverse proxy to
+    // prevent any files that should not be instrumented from even reaching the
+    // Intern proxy.
+
+    // WARNING - PhantomJS does not seem to like [Istanbul code coverage /
+    // large JS files] combination.  Thus you may find the PhantomJS tests fail
+    // when testing the built "webapp-build" files with instrumentation turned
+    // on.  In this case you can set excludeInstrumentation to true, to
+    // completely disable instrumentation.
+    // See https://theintern.github.io/intern/#option-excludeInstrumentation
+    excludeInstrumentation: new RegExp([
+        // Exclude Intern's own files
+        '^intern/',
+
+        // Exclude test files
+        '^tests/',
+
+        // Exclude any 3rd party components installed via package manager
+        '^bower_components/',
+        '^node_modules/',
+
+        // Exclude any 3rd party components in HAR Viewer's own source
+        'scripts/jquery.js$',
+        'scripts/require.js$',
+        'scripts/text.js$',
+        'scripts/i18n.js$',
+
+        // Exclude any 3rd party components in HAR Viewer's own source
+        '/downloadify/',
+        '/json-query/',
+        '/syntax-highlighter/'
+      ].join('|'))
   };
 
   return config;
