61ae53c6f0388593a7cd890fa25d5326b2dab6c2
gitgrimbo <gitgrimbo@gmail.com>
2016-05-28 09:21:43 +0100

    Upgrade test dependencies
    - Intern 3.0.0 to 3.2.3.
    - Selenium 2.47.1 to 2.53.0.
    - IEDriver 2.47.0 to 2.53.1.
    - ChromeDriver 2.16 to 2.21.
    - PhantomJS 2.0.0 to 2.1.1.
    - JSDoc 3.3.2 to 3.4.0.
    - Update intern.js accordingly.

--- tests/intern.js
+++ tests/intern.js
@@ -5,67 +5,70 @@
  * config for functional testing.
  */
 define([
-], function() {
-  var config = {
+  'intern/dojo/has'
+], function(has) {
+  var config = {};
 
-    defaultTimeout: 60 * 1000,
+  config.defaultTimeout = 60 * 1000;
 
-    reporters: [
-      'Pretty',
-      'Lcov',
-      'LcovHtml',
-      { 'id': 'JUnit', 'filename': 'report.xml' }
-    ],
+  config.reporters = [
+    'Pretty',
+    'Lcov',
+    'LcovHtml',
+    { 'id': 'JUnit', 'filename': 'report.xml' }
+  ];
 
-    // Configuration options for the module loader; any AMD configuration options supported by the Dojo loader can be
-    // used here
-    loaderOptions: {
-      packages: [{name: 'core', location: 'webapp/scripts/core'}]
-    },
-
-    // Non-functional test suite(s) to run in each browser
-    suites: [
-      'intern/node_modules/dojo/has!host-browser?tests/unit/unit',
-      'tests/unit/core/lib',
-      'tests/unit/core/cookies'
-    ],
-
-    // A regular expression matching URLs to files that should not be included
-    // in code coverage analysis.
-
-    // Some of the patterns listed here can also be used in a reverse proxy to
-    // prevent any files that should not be instrumented from even reaching the
-    // Intern proxy.
-
-    // WARNING - PhantomJS does not seem to like [Istanbul code coverage /
-    // large JS files] combination.  Thus you may find the PhantomJS tests fail
-    // when testing the built "webapp-build" files with instrumentation turned
-    // on.  In this case you can set excludeInstrumentation to true, to
-    // completely disable instrumentation.
-    // See https://theintern.github.io/intern/#option-excludeInstrumentation
-    excludeInstrumentation: new RegExp([
-        // Exclude Intern's own files
-        '^intern/',
-
-        // Exclude test files
-        '^tests/',
-
-        // Exclude any 3rd party components installed via package manager
-        '^bower_components/',
-        '^node_modules/',
-
-        // Exclude any 3rd party components in HAR Viewer's own source
-        'scripts/jquery.js$',
-        'scripts/require.js$',
-        'scripts/text.js$',
-        'scripts/i18n.js$',
-
-        // Exclude any 3rd party components in HAR Viewer's own source
-        '/downloadify/',
-        '/json-query/',
-        '/syntax-highlighter/'
-      ].join('|'))
+  // Configuration options for the module loader; any AMD configuration options supported by the Dojo loader can be
+  // used here
+  config.loaderOptions = {
+    packages: [{name: 'core', location: 'webapp/scripts/core'}]
   };
+
+  // Non-functional test suite(s) to run in each browser
+  config.suites = [];
+  if (has('host-browser')) {
+    config.suites.push('tests/unit/unit');
+  }
+  config.suites = config.suites.concat([
+    'tests/unit/core/lib',
+    'tests/unit/core/cookies'
+  ]);
+
+  // A regular expression matching URLs to files that should not be included
+  // in code coverage analysis.
+
+  // Some of the patterns listed here can also be used in a reverse proxy to
+  // prevent any files that should not be instrumented from even reaching the
+  // Intern proxy.
+
+  // WARNING - PhantomJS does not seem to like [Istanbul code coverage /
+  // large JS files] combination.  Thus you may find the PhantomJS tests fail
+  // when testing the built "webapp-build" files with instrumentation turned
+  // on.  In this case you can set excludeInstrumentation to true, to
+  // completely disable instrumentation.
+  // See https://theintern.github.io/intern/#option-excludeInstrumentation
+  config.excludeInstrumentation = new RegExp([
+      // Exclude Intern's own files
+      '^intern/',
+
+      // Exclude test files
+      '^tests/',
+
+      // Exclude any 3rd party components installed via package manager
+      '^bower_components/',
+      '^node_modules/',
+
+      // Exclude any 3rd party components in HAR Viewer's own source
+      'scripts/jquery.js$',
+      'scripts/require.js$',
+      'scripts/text.js$',
+      'scripts/i18n.js$',
+
+      // Exclude any 3rd party components in HAR Viewer's own source
+      '/downloadify/',
+      '/json-query/',
+      '/syntax-highlighter/'
+    ].join('|'));
 
   return config;
 });
