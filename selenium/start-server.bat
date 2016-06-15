0d80861ef2947f67784af7ffbe6d9b20743201ae
gitgrimbo <gitgrimbo@gmail.com>
2016-06-15 11:17:21 +0100

    Add geckodriver for >=FF47
    Need to use the new geckodriver for FF47 and later.
    
    References:
    
    - https://github.com/SeleniumHQ/selenium/issues/2110
    - https://developer.mozilla.org/en-US/docs/Mozilla/QA/Marionette/WebDriver
    - https://github.com/mozilla/geckodriver

--- selenium/start-server.bat
+++ selenium/start-server.bat
@@ -1,18 +1,20 @@
 REM Assumes IEDriverServer.exe and chromedriver.exe are in the same folder as this batch file.
 
-set          FIREFOX_EXE=c:\Program Files\Mozilla Firefox\firefox.exe
+set          FIREFOX_EXE=c:\apps\Mozilla Firefox\46\firefox.exe
 set        IE_DRIVER_EXE=%~dp0IEDriverServer.exe
 set    CHROME_DRIVER_EXE=%~dp0chromedriver.exe
 set        PHANTOMJS_EXE=c:\apps\phantomjs\2.1.1\bin\phantomjs.exe
+set     GECKO_DRIVER_EXE=%~dp0geckodriver.exe
 
 set    CHROME_DRIVER_ARG=-Dwebdriver.chrome.driver="%CHROME_DRIVER_EXE%"
 set        IE_DRIVER_ARG=-Dwebdriver.ie.driver="%IE_DRIVER_EXE%"
 set   FIREFOX_DRIVER_ARG=-Dwebdriver.firefox.bin="%FIREFOX_EXE%"
 set PHANTOMJS_DRIVER_ARG=-Dphantomjs.binary.path="%PHANTOMJS_EXE%"
+set     GECKO_DRIVER_ARG=-Dwebdriver.gecko.driver="%GECKO_DRIVER_EXE%"
 
-set  FIREFOX_PROFILE_ARG=-Dwebdriver.firefox.profile=selenium
+set  FIREFOX_PROFILE_ARG=-Dwebdriver.firefox.profile=ff46-selenium
 
-set DRIVER_ARGS=%FIREFOX_DRIVER_ARG% %CHROME_DRIVER_ARG% %IE_DRIVER_ARG% %PHANTOMJS_DRIVER_ARG% %FIREFOX_PROFILE_ARG%
+set DRIVER_ARGS=%FIREFOX_DRIVER_ARG% %CHROME_DRIVER_ARG% %IE_DRIVER_ARG% %PHANTOMJS_DRIVER_ARG% %GECKO_DRIVER_ARG% %FIREFOX_PROFILE_ARG%
 
 REM java -jar server/selenium-server.jar -debug
 java -jar %~dp0server\selenium-server.jar %DRIVER_ARGS% -debug
