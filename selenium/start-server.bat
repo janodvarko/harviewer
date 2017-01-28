5c6cee2cce274a413c10a388e5400d996feaefc3
gitgrimbo <gitgrimbo@gmail.com>
2017-01-28 09:27:11 +0000

    Update deps ...
    r.js, chromedriver, geckodriver, Firefox.
    
    | dep          | old    | new    |
    | ------------ | ------ | ------ |
    | r.js         | 2.2.0  | 2.3.2  |
    | chromedriver | 2.24   | 2.27   |
    | geckodriver  | 0.11.1 | 0.13.0 |
    | Firefox      | 46     | 47     |
    
    Test results:
    
    ```
    tests/intern-selenium-standalone
    Tunnel: Starting
    
    Total: [] 520/520
    Passed: 520  Failed: 0    Skipped: 0
    
    Chr 55 XP:  [] 130/130
    Phan 2 XP:  [] 130/130
    IE 11 WIN:  [] 130/130
    Fx 47 WIN:  [] 130/130
    
    --------------------|----------|----------|----------|----------|----------------|
    File                |  % Stmts | % Branch |  % Funcs |  % Lines |Uncovered Lines |
    --------------------|----------|----------|----------|----------|----------------|
     core\              |    41.01 |    17.38 |    30.94 |     41.3 |                |
      StatsService.js   |    88.03 |    72.92 |      100 |    88.03 |... 277,294,295 |
      array.js          |    34.78 |        0 |       40 |    34.78 |... 71,73,74,77 |
      cookies.js        |    14.29 |        0 |    14.29 |    14.29 |... 1,92,93,104 |
      css.js            |    23.81 |        0 |    11.11 |    23.81 |... 123,125,126 |
      date.js           |    65.12 |    43.75 |       60 |    68.29 |... 7,98,99,102 |
      dom.js            |    12.87 |        0 |        5 |    12.87 |... 351,352,355 |
      events.js         |    32.76 |        0 |     5.88 |    32.76 |... 187,195,203 |
      json.js           |    84.62 |      100 |      100 |    84.62 |          40,43 |
      lib.js            |    53.33 |        0 |       50 |    53.33 |... 112,113,114 |
      mime.js           |      100 |      100 |      100 |      100 |                |
      object.js         |    43.33 |        0 |       25 |    43.33 |... 80,81,82,83 |
      rect.js           |    71.43 |        0 |    33.33 |    71.43 |          39,54 |
      sniff.js          |      100 |    64.29 |      100 |      100 |                |
      string.js         |    23.47 |     1.82 |    14.29 |    24.47 |... 225,227,238 |
      trace.js          |       45 |     8.33 |     9.09 |       45 |... 45,46,51,52 |
      url.js            |    34.56 |       20 |    35.29 |    34.56 |... 337,348,349 |
     nls\               |      100 |      100 |      100 |      100 |                |
      harModel.js       |      100 |      100 |      100 |      100 |                |
     preview\           |    30.23 |    10.98 |    41.82 |    30.23 |                |
      harModel.js       |    29.71 |    23.17 |    40.91 |    29.71 |... 379,380,383 |
      harModelLoader.js |    67.53 |    48.98 |    53.33 |    67.53 |... 253,255,258 |
      harSchema.js      |      100 |       75 |      100 |      100 |                |
      jsonSchema.js     |     6.36 |        0 |     12.5 |     6.36 |... 212,213,215 |
      ref.js            |     4.65 |        0 |    16.67 |     4.65 |... 194,196,200 |
    --------------------|----------|----------|----------|----------|----------------|
    All files           |    37.33 |    14.18 |    34.02 |    37.51 |                |
    --------------------|----------|----------|----------|----------|----------------|
    ```

--- selenium/start-server.bat
+++ selenium/start-server.bat
@@ -1,6 +1,6 @@
 REM Assumes IEDriverServer.exe and chromedriver.exe are in the same folder as this batch file.
 
-set          FIREFOX_EXE=c:\apps\Mozilla Firefox\46\firefox.exe
+set          FIREFOX_EXE=c:\apps\Mozilla Firefox\47\firefox.exe
 set        IE_DRIVER_EXE=%~dp0IEDriverServer.exe
 set    CHROME_DRIVER_EXE=%~dp0chromedriver.exe
 set        PHANTOMJS_EXE=c:\apps\phantomjs\2.1.1\bin\phantomjs.exe
@@ -12,7 +12,7 @@ set   FIREFOX_DRIVER_ARG=-Dwebdriver.firefox.bin="%FIREFOX_EXE%"
 set PHANTOMJS_DRIVER_ARG=-Dphantomjs.binary.path="%PHANTOMJS_EXE%"
 set     GECKO_DRIVER_ARG=-Dwebdriver.gecko.driver="%GECKO_DRIVER_EXE%"
 
-set  FIREFOX_PROFILE_ARG=-Dwebdriver.firefox.profile=ff46-selenium
+set  FIREFOX_PROFILE_ARG=-Dwebdriver.firefox.profile=ff47
 
 set DRIVER_ARGS=%FIREFOX_DRIVER_ARG% %CHROME_DRIVER_ARG% %IE_DRIVER_ARG% %PHANTOMJS_DRIVER_ARG% %GECKO_DRIVER_ARG% %FIREFOX_PROFILE_ARG%
 
